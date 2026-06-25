ab0c7ae206673039a9f96b1debbf23d1ec1ddb6d
Nilabh Kishore Gupta <spycoder07@gmail.com>
2026-06-25 11:12:35 +0530

    Add: is-anagram - submission-4

---
+++ Data Structures & Algorithms/is-anagram/submission-4.cpp
@@ -0,0 +1,19 @@
+class Solution {
+   public:
+    bool isAnagram(string s, string t) {
+        // hashing se krte hai
+        if (s.size() != t.size()) {
+            return false;
+        }
+
+        unordered_map<char,int> countS;
+        unordered_map<char,int> countT;
+        
+        for(int i = 0;i < s.size();i++){
+            countS[s[i]]++;
+            countT[t[i]]++;
+        }
+
+        return (countS == countT);
+    }
+};
