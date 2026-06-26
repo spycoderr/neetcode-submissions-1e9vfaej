a9869b661fc7c4d393b54021cc3539e1edf69acd
Nilabh Kishore Gupta <spycoder07@gmail.com>
2026-06-26 16:55:38 +0530

    Add: is-anagram - submission-6

---
+++ Data Structures & Algorithms/is-anagram/submission-6.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        sort(s.begin(),s.end());
+        sort(t.begin(),t.end());
+        int n=s.size();
+        int m=t.size();
+        if(n!=m){
+            return false;
+        }
+        for(int i=0;i<=s.size()-1;i++){
+            if(s[i]!=t[i]){
+                return false;
+            }
+            
+        }
+        return true;
+    }
+};
