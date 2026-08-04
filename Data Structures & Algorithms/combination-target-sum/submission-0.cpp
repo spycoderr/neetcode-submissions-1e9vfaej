bb8158dfc97790c064221976023dc6f50f2ef9c8
Nilabh Kishore Gupta <spycoder07@gmail.com>
2026-08-04 19:13:14 +0530

    Add: combination-target-sum - submission-0

---
+++ Data Structures & Algorithms/combination-target-sum/submission-0.cpp
@@ -0,0 +1,30 @@
+class Solution {
+   public:
+    void combination(int target, vector<int>& nums,vector<int> temp,
+        vector<vector<int>>& ans,int index, int n) {
+        if (target < 0 || index >= n) {
+            return;
+        }
+
+        if (target == 0) {
+            ans.push_back(temp);
+            return;
+        }
+
+        combination(target,nums,temp,ans,index+1,n);
+        temp.push_back(nums[index]);
+        combination(target - nums[index],nums,temp,ans,index,n);
+    }
+
+    vector<vector<int>> combinationSum(vector<int>& nums, int target) {
+        // add krte rho and kabhi bhi target < 0 hogya matlab
+        // return krjao and = 0 hogya then pushback in answer
+
+        vector<int> temp;
+        vector<vector<int>> ans;
+        int n = nums.size();
+
+        combination(target,nums,temp,ans,0,n);
+        return ans;
+    }
+};
