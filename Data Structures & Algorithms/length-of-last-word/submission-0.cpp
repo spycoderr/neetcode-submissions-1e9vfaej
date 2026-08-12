d29730ffefe5c2d11b6bfed74d51e0dfa0c0b7ba
Nilabh Kishore Gupta <spycoder07@gmail.com>
2026-08-12 10:31:55 +0530

    Add: length-of-last-word - submission-0

---
+++ Data Structures & Algorithms/length-of-last-word/submission-0.cpp
@@ -0,0 +1,16 @@
+class Solution {
+   public:
+    int lengthOfLastWord(string s) {
+        int count = 0;
+        for (int i = s.size() - 1; i >= 0; i--) { 
+            if (s[i] == ' ' && count == 0) {       
+                continue;                       
+            } else if (s[i] == ' ' && count > 0) {
+                break; 
+            } else if (s[i] != ' ') {
+                count++;  
+            }
+        }
+        return count;
+    }
+};
